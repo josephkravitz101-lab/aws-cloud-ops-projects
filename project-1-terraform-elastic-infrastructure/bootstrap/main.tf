3ad671f3353739ee0d23435c7453d791fe429697
Joseph Kravitz <josephkravitz101@gmail.com>
2026-05-07 16:24:21 -0400

    Code refactoring

--- project-1-terraform-elastic-infrastructure/bootstrap/main.tf
+++ project-1-terraform-elastic-infrastructure/bootstrap/main.tf
@@ -30,3 +30,43 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
+
+# 1. The "Identity Provider" - The digital handshake between AWS and GitHub
+resource "aws_iam_openid_connect_provider" "github" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  # Standard thumbprint for GitHub's certificate
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+# 2. The "Role" - The set of permissions GitHub will "wear"
+resource "aws_iam_role" "github_actions_role" {
+  name = "GitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Condition = {
+        # IMPORTANT: This ensures ONLY your repo can use this role
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:josephkravitz101-lab/aws-cloud-ops-projects:*"
+        }
+      }
+    }]
+  })
+}
+
+# 3. The "Permissions" - Allowing the role to manage your resources
+resource "aws_iam_role_policy_attachment" "gha_admin" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
