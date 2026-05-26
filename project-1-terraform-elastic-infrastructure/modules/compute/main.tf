757e4c9f26bd301c9aaab04018eed54b313e9f80
Joseph Kravitz <josephkravitz101@gmail.com>
2026-05-25 21:55:29 -0400

    Updated Cloudwatch

--- project-1-terraform-elastic-infrastructure/modules/compute/main.tf
+++ project-1-terraform-elastic-infrastructure/modules/compute/main.tf
@@ -80,21 +80,22 @@ resource "aws_sns_topic_subscription" "email_subscription" {
 }
 
 # CloudWatch Alarm
-# resource "aws_cloudwatch_metric_alarm" "cpu_high" {
-#   alarm_name          = "cpu-utilization-high"
-#   comparison_operator = "GreaterThanThreshold"
-#   evaluation_periods  = 2
-#   metric_name         = "CPUUtilization"
-#   namespace           = "AWS/EC2"
-#   period              = 300
-#   statistic           = "Average"
-#   threshold           = 70
-#   alarm_actions = [
-#     aws_sns_topic.cpu_alarm_topic.arn,   # Email alert
-#     aws_autoscaling_policy.scale_out.arn # Triggers the ASG to scale
-#   ]
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "cpu-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "GroupAverageCPUUtilization"
+  namespace           = "AWS/AutoScaling"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 70
 
-#   dimensions = {
-#     AutoScalingGroupName = aws_autoscaling_group.web_asg.name
-#   }
-# }
+  alarm_actions = [
+    aws_sns_topic.cpu_alarm_topic.arn
+  ]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web_asg.name
+  }
+}
+
